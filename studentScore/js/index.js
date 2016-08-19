d2aac5ffee3494c2879cfad957c28bcc3dbdcc8e
heronlyj <heronlyj@gmail.com>
2016-08-19 12:00:05 +0800

    save

--- studentScore/js/index.js
+++ studentScore/js/index.js
@@ -1,6 +1,12 @@
 
 
+// 匹配成绩
 var checkSourceFunc = function(sourceNum) {
+    // 非空判断
+    if (sourceNum == "") {
+        return "请输入内容";
+    }
+
     switch (true) {
         case sourceNum >= 90 && sourceNum <= 100:
             return "1等生";
@@ -10,15 +16,17 @@ var checkSourceFunc = function(sourceNum) {
             return "3等生";
         case sourceNum >= 60 && sourceNum < 70:
             return "4等生";
-        case sourceNum < 60:
+        case sourceNum >= 0 && sourceNum < 60:
             return "不及格";
         default:
             return "您的成绩不符合逻辑,建议重考";
     }
 };
 
+// 获得提交 button
 var btn = document.getElementById('checkBtn');
 
+// 点击事件
 btn.onclick = function () {
     var sorceInput = document.getElementById('sorceInput');
     var source = sorceInput.value;
